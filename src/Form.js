d3755975c38375831e3aa7c3d75f485da18b7db2
seows <seows@laivdata.com>
2021-11-26 19:04:58 +0900

    Form을 useState로 구현

--- src/Form.js
+++ src/Form.js
@@ -1,18 +1,27 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { useTodoDispatch } from './TodoProvider';
 
 function Form() {
   const dispatch = useTodoDispatch();
-  const inputRef = useRef();
+  //   const inputRef = useRef();
+  const [name, setName] = useState('');
   function onSubmit(event) {
     event.preventDefault();
-    dispatch({ type: 'ADD', payload: { name: inputRef.current.value } });
-    inputRef.current.value = '';
+    // dispatch({ type: 'ADD', payload: { name: inputRef.current.value } });
+    dispatch({ type: 'ADD', payload: { name } });
+    // inputRef.current.value = '';
+    setName('');
   }
   console.log('Form rendered');
   return (
     <form onSubmit={onSubmit}>
-      <input ref={inputRef} />
+      {/* <input ref={inputRef} /> */}
+      <input
+        value={name}
+        onChange={(e) => {
+          setName(e.target.value);
+        }}
+      />
       <button type="submit">등록</button>
     </form>
   );
