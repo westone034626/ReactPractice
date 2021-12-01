f92170880fd30486ab1e94dd163411c502a6cd7f
seows <seows@laivdata.com>
2021-12-01 10:43:00 +0900

    Carousel.js에 주석 추가

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -10,24 +10,30 @@ export default function Carousel() {
   const [index, setIndex] = useState(0);
   return (
     <>
+      {/* 캐러셀의 viewport */}
       <div
         style={{
           width: '100%',
           maxWidth: '1000px',
+          // *overflow: hidden을 아래 div에 넣으면 이미지가 1개만 보이게 됨
           overflow: 'hidden',
           margin: 'auto',
           position: 'relative',
         }}
       >
+        {/* 캐러셀의 내용물을 전부 포함하고 있는 부분(해당 코드에서는 3개의 이미지를 담고 있음) */}
         <div
           style={{
             width: '100%',
+            // 각각의 cardImage에 flexShrink: 0을 설정해놓았음
             display: 'flex',
+            // transistion이나 animation과의 조합을 생각했을 때 transform이 나음
             transform: `translate(-${index * 100}%)`,
             transition: 'transform 0.5s',
           }}
         >
           {arr.map((i, index) => (
+            // 캐러셀 내에서 표현하려는 최소 단위 내용물(1개 이미지)
             <div
               key={index}
               style={{ backgroundImage: `url(${i})` }}
@@ -35,6 +41,7 @@ export default function Carousel() {
             ></div>
           ))}
         </div>
+        {/* 캐러셀의 viewport 위에 덧씌워진 layer(좌우 화살표 버튼 배치용) */}
         <div
           style={{
             position: 'absolute',
@@ -48,6 +55,7 @@ export default function Carousel() {
             padding: '20px',
           }}
         >
+          {/* 좌 버튼 */}
           <button
             style={{
               width: '10vw',
@@ -63,6 +71,7 @@ export default function Carousel() {
           >
             {'<'}
           </button>
+          {/* 우 버튼 */}
           <button
             style={{
               width: '10vw',
@@ -80,6 +89,7 @@ export default function Carousel() {
           </button>
         </div>
       </div>
+      {/* 캐러셀을 버튼으로 조작하는 컨트롤러 */}
       <div style={{ display: 'flex', justifyContent: 'center' }}>
         {arr.map((i, index) => (
           <button
